1eaa27a783f98967b7e8ed07b62f27f97a8d0a5a
glovek08 <gbrielbbarn@gmail.com>
2025-07-23 22:19:15 -0300

    feat: navbar and footer components, static css styling for dark/light theme

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,3 @@
+<footer>
+  <a href="https://www.flaticon.com/free-icons/app" title="app icons">App icons created by Freepik - Flaticon</a>
+</footer>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import '../app.css';
+  import Footer from '$lib/components/Footer.svelte';
+  import Header from '$lib/components/Navbar.svelte';
+</script>
+
+<Header />
+
+<main>
+  <slot /> <!-- This is where +page.svelte content goes -->
+</main>
+
+<Footer />
